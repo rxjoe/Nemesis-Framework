a488ddeaf1a265773da947004e08b75cf9334cfa
rxjoe <rxstoreaistore@gmail.com>
2026-07-03 02:30:11 +0300

    Update default.lua

---
+++ ui/themes/default.lua
@@ -0,0 +1,7 @@
+local Theme = {
+    Background = Color3.fromRGB(15, 15, 20),
+    Header = Color3.fromRGB(25, 25, 32),
+    Primary = Color3.fromRGB(200, 50, 255), -- بنفسجي
+    Text = Color3.fromRGB(220, 220, 220)
+}
+return Theme
